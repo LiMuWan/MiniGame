f33b6c200bc88107304c63aaa1562ed319b0153a
Liruirui <18335929360@163.com>
2023-05-19 08:41:50 +0800

    LoadDll整理

--- Assets/Main/Runtime/LoadDll/LoadDll.cs
+++ Assets/Main/Runtime/LoadDll/LoadDll.cs
@@ -7,6 +7,7 @@ using YooAsset;
 using System.Reflection;
 using System;
 using GameFramework.Resource;
+using HybridCLR;
 
 public class LoadDll : SingletonInstance<LoadDll>, ISingleton
 {
@@ -19,6 +20,14 @@ public class LoadDll : SingletonInstance<LoadDll>, ISingleton
         "System.dll",
         "System.Core.dll",
     };
+
+
+    // TODO 可以用配置文件
+    public static List<string> HotfixAssemblyNames {get;} = new List<string>()
+    {
+       "Assembly-CSharp.dll",
+       "Hotfix.dll",
+    };     
 
     private static Dictionary<string,byte[]> assetDatas = new Dictionary<string, byte[]>();
 
@@ -45,29 +54,47 @@ public class LoadDll : SingletonInstance<LoadDll>, ISingleton
     public IEnumerator LoadDllAsset()
     {
         yield return LoadMetadataForAOTAssemblies();
+        yield return LoadHotfixAssemblies();
+        Debug.Log("LoadHotfixAssemblies Finish!!!");
         Assembly hotUpdateAss = Assembly.Load(GetAssetData("Hotfix.dll"));
+        Debug.Log("Assembly.Load(Hotfix.dll)) Finish!!!");
         Type type = hotUpdateAss.GetType("Main");
         type.GetMethod("Run").Invoke(null, null);
+         Debug.Log("Main Run Finish!!!");
     }
 
+    /// <summary>
+    /// 为aot assembly加载原始metadata， 这个代码放aot或者热更新都行。
+    /// 一旦加载后，如果AOT泛型函数对应native实现不存在，则自动替换为解释模式执行
+    /// </summary>
     private IEnumerator LoadMetadataForAOTAssemblies()
     {
-        var package = YooAssets.GetPackage("DefaultPackage");
-
-        var assets = new List<string>
+        /// 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
+        /// 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误
+        /// 
+        HomologousImageMode mode = HomologousImageMode.SuperSet;
+        foreach (var aotDllName in AOTMetaAssemblyNames)
         {
-            "Assembly-CSharp.dll",
-            "UIFrame.dll",
-            "Hotfix.dll",
-        }.Concat(AOTMetaAssemblyNames);
-
-        foreach (var asset in assets)
-        {
-            RawFileOperationHandle handle = ResourcesManager.Instance.LoadRawFileAsync(asset);
+            var handle = ResourcesManager.Instance.LoadRawFileAsync(aotDllName);
             yield return handle;
-            byte[] fileData = handle.GetRawFileData();
-            assetDatas[asset] = fileData;
-            Debug.Log($"dll:{asset} size : {fileData.Length}");
+            byte[] dllBytes = handle.GetRawFileData();
+            assetDatas[aotDllName] = dllBytes;
+            // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
+            int err = (int)HybridCLR.RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, mode);
+            Debug.Log($"LoadMetadataForAOTAssembly:{aotDllName}. mode:{mode} ret:{err}");
         }
     }
+    
+    private IEnumerator LoadHotfixAssemblies()
+    {
+        foreach (var hotfixDllName in HotfixAssemblyNames)
+        {
+            var handle = ResourcesManager.Instance.LoadRawFileAsync(hotfixDllName);
+            yield return handle;
+            byte[] dllBytes = handle.GetRawFileData();
+            assetDatas[hotfixDllName] = dllBytes;
+            Debug.Log($"hotfixDllName:{hotfixDllName}");
+        }
+    }
+    
 }
--- Assets/Main/Runtime/ResourcesLoad/ResourcesManager.cs
+++ Assets/Main/Runtime/ResourcesLoad/ResourcesManager.cs
@@ -180,7 +180,7 @@ namespace GameFramework.Resource
 
         private string GetHostServerURL()
         {
-            string hostServerIP = "http://192.168.1.15";
+            string hostServerIP = "http://192.168.0.111";
             string gameVersion = "version";
 
 #if UNITY_EDITOR

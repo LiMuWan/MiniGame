a6b55aa2c712f6954b45b65ec5bbd5b5cdd664fd
bling01\LiRui <18335929360@163.com>
2023-06-14 10:36:25 +0800

    主页调整

--- Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -8,6 +8,7 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// UniFramework.Event.dll
 	// UniFramework.Singleton.dll
 	// UnityEngine.CoreModule.dll
+	// YooAsset.dll
 	// mscorlib.dll
 	// }}
 
@@ -15,23 +16,32 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// }} 
 
 	// {{ AOT generic types
+	// System.Action<UnityEngine.Vector4>
 	// System.Action<object>
 	// System.Action<int>
-	// System.Collections.Generic.Dictionary<object,object>
+	// System.Action<UnityEngine.Color>
+	// System.Action<UnityEngine.Quaternion>
+	// System.Action<UnityEngine.Vector3>
+	// System.Action<UnityEngine.Vector2>
+	// System.Action<float>
 	// System.Collections.Generic.Dictionary<int,object>
+	// System.Collections.Generic.Dictionary<object,object>
+	// System.Collections.Generic.Dictionary<object,UnityEngine.SceneManagement.Scene>
 	// System.Collections.Generic.HashSet<object>
 	// System.Collections.Generic.IEnumerable<object>
 	// System.Collections.Generic.IEnumerator<object>
+	// System.Collections.Generic.List<object>
 	// System.Collections.Generic.List<float>
-	// System.Collections.Generic.List<int>
 	// System.Collections.Generic.List<UniFramework.Window.UniWindow.WindowInfo>
 	// System.Collections.Generic.List<UnityEngine.Vector2>
 	// System.Collections.Generic.List<byte>
-	// System.Collections.Generic.List<object>
+	// System.Collections.Generic.List<int>
 	// System.Collections.Generic.List.Enumerator<object>
 	// System.Collections.Generic.Queue<object>
 	// System.Comparison<object>
+	// System.Func<byte>
 	// System.Func<object,object>
+	// System.Func<object,float>
 	// System.Func<object,int,byte>
 	// System.Nullable<double>
 	// System.Predicate<object>
@@ -43,12 +53,13 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	public void RefMethods()
 	{
 		// string Bright.Common.StringUtil.CollectionToString<int>(System.Collections.Generic.IEnumerable<int>)
-		// YooAsset.AssetOperationHandle GameFramework.Resource.ResourcesManager.LoadAssetAsync<object>(string)
 		// System.Void GameFramework.Resource.ResourcesManager.LoadAssetAsync<object>(string,System.Action<object>)
+		// YooAsset.AssetOperationHandle GameFramework.Resource.ResourcesManager.LoadAssetAsync<object>(string)
 		// System.Void GameFramework.Resource.ResourcesManager.LoadSubAssetsAsync<object>(string,System.Action<object>)
 		// object LitJson.JsonMapper.ToObject<object>(string)
 		// object System.Activator.CreateInstance<object>()
 		// object[] System.Array.Empty<object>()
+		// System.Linq.IOrderedEnumerable<object> System.Linq.Enumerable.OrderByDescending<object,float>(System.Collections.Generic.IEnumerable<object>,System.Func<object,float>)
 		// System.Collections.Generic.IEnumerable<object> System.Linq.Enumerable.Take<object>(System.Collections.Generic.IEnumerable<object>,int)
 		// System.Collections.Generic.List<object> System.Linq.Enumerable.ToList<object>(System.Collections.Generic.IEnumerable<object>)
 		// System.Void UniFramework.Event.EventGroup.AddListener<object>(System.Action<UniFramework.Event.IEventMessage>)
@@ -62,6 +73,6 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 		// object[] UnityEngine.GameObject.GetComponentsInChildren<object>(bool)
 		// object UnityEngine.Object.FindObjectOfType<object>()
 		// object UnityEngine.Object.Instantiate<object>(object)
-		// object UnityEngine.Object.Instantiate<object>(object,UnityEngine.Transform)
+		// YooAsset.AssetOperationHandle YooAsset.ResourcePackage.LoadAssetAsync<object>(string)
 	}
 }

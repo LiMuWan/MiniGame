95276d778f27a6515c06577168606c32463bb1cb
bling01\LiRui <18335929360@163.com>
2023-06-21 12:53:59 +0800

    属性面板修改

--- Assets/HotfixScripts/Runtime/HotfixBusiness/UI/UIWindow/UIItemInfo.cs
+++ Assets/HotfixScripts/Runtime/HotfixBusiness/UI/UIWindow/UIItemInfo.cs
@@ -83,7 +83,7 @@ public partial class UIItemInfo
         old_item_lv.text = $"Lv.{itemData.Level}";
         old_quality_text.text = qualityConfig.Title;
         Color color = ColorTool.HtmlStringToColor(qualityConfig.Color);
-        old_item_lv.color = color;
+        old_item_name.color = color;
         old_quality_text.color = color;
         ResourcesManager.Instance.LoadAssetAsync<Texture>(qualityConfig.Icon, (texture) => { old_quality_icon.texture = texture; });
         ResourcesManager.Instance.LoadAssetAsync<Texture>(item_icon, (texture) => { old_item_icon.texture = texture; });
@@ -113,7 +113,7 @@ public partial class UIItemInfo
         new_item_lv.text = $"Lv.{itemData.Level}";
         new_quality_text.text = qualityConfig.Title;
         Color color = ColorTool.HtmlStringToColor(qualityConfig.Color);
-        old_item_lv.color = color;
+        new_item_name.color = color;
         old_quality_text.color = color;
         new_PropertiesTableManager.Count = 4;
         new_PropertiesTableManager.Each(OnEachProperties);
